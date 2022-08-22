8471568ece5ba004493d02c5d6b1cd935e423c4a
Matthew Thomas <19608881+matthewllthomas@users.noreply.github.com>
2022-08-22 14:46:53 +0100

    Adding file for source code

---
+++ Code/CaseStudy2/0_Source.R
@@ -0,0 +1,30 @@
+################################################################################################
+### This file contains all source code needed for the the Data Integration Model for personal###
+### exposures (DIMEX)                                                                        ###
+################################################################################################
+
+#################################
+### Loading libraryd packages ###
+#################################
+# import libraries
+library(DescTools)
+library(doParallel)  
+library(dplyr)
+library(EnvStats)    
+library(ggmap)  
+library(haven)
+library(lhs) 
+library(lubridate)  
+library(maptools)
+library(openair)
+library(parallel)
+library(plyr)
+library(raster)
+library(readxl)
+library(reshape2)
+library(rgeos)    
+library(rgdal)
+library(sf)
+library(tidyverse)  
+library(truncnorm) 
+library(zoo)
